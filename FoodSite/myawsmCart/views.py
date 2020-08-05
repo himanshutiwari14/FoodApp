285cc8ab1af8c8b4e1bdd50594abbeecf979f607
Himanshu <tiwarihimanshu4556@gmail.com>
2020-08-05 22:03:34 +0530

    minor changes

--- FoodSite/myawsmCart/views.py
+++ FoodSite/myawsmCart/views.py
@@ -3,21 +3,15 @@ from django.shortcuts import HttpResponse
 from . models import Products
 import math
 def index(request):
-    prod=Products.objects.all()
-    print(len(prod))
-    if len(prod)==0:
+    products_aval=Products.objects.all()
+    if len(products_aval)==1:
         slides_required=1
-
-    if len(prod)%4==0:
-        slides_required=len(prod)//4
-
-    slides_required=len(prod)// 4 + math.ceil((len(prod) / 4) - (len(prod) // 4))
-    print(slides_required,"sr")
-    all_prods=[[prod,range(1,len(prod),slides_required)],[prod,range(1,len(prod),slides_required)]]
-
-    params = {'all_prods':all_prods}
-   
-    return render(request,'myawsmCart/index3.html',params)
+    if len(products_aval)%3==0:
+        slides_required=len(products_aval)//3
+    else:
+        slides_required=(len(products_aval)//3)+1
+    params={"number_of_produsts":len(products_aval),"actual_prodcuts":products_aval,"range":range(1,slides_required)}
+    return render(request,'myawsmCart/index.html',params)
 
 def about_us(request):
     return render(request,'myawsmCart/index2.html')
