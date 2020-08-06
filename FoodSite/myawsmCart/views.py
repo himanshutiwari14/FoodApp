a21f8ceed5d6431cde8b97a5261f770aaf766095
Himanshu <tiwarihimanshu4556@gmail.com>
2020-08-06 21:42:39 +0530

    multiple slides

--- FoodSite/myawsmCart/views.py
+++ FoodSite/myawsmCart/views.py
@@ -10,7 +10,9 @@ def index(request):
         slides_required=len(products_aval)//3
     else:
         slides_required=(len(products_aval)//3)+1
-    params={"number_of_produsts":len(products_aval),"actual_prodcuts":products_aval,"range":range(1,slides_required)}
+    list_all_prods= [[products_aval, range(1, slides_required), slides_required],[products_aval, range(1, slides_required), slides_required]]
+    # params={"number_of_produsts":len(products_aval),"actual_prodcuts":products_aval,"range":range(1,slides_required)}
+    params={"all_prods":list_all_prods}
     return render(request,'myawsmCart/index.html',params)
 
 def about_us(request):
